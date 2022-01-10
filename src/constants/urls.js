9d7d133d0dbb1d7c58edcd59af7f665332eb6d78
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2022-01-10 17:19:22 +0700

    Revisi Tampilan

--- src/constants/urls.js
+++
@@ -1,7 +0,0 @@
-/*
-  Ini constant Url, buat ngedefine Url rest API biasa
-*/
-
-export const BASE_URL = "http://192.168.43.245:8000";
-
-export const GET_FILMS = `${BASE_URL}/films`;
--- src/urls.js
+++
@@ -1,7 +0,0 @@
-/*
-  Ini constant Url, buat ngedefine Url rest API biasa
-*/
-
-const BASE_URL = "https://ghibliapi.herokuapp.com";
-
-export const GET_FILMS = `${BASE_URL}/films`;
